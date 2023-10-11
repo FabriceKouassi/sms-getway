87d63879beab79e330859e6b51dc44b0ee541589
FabriceKouassi <fabrice.ako@dkbsolutions.com>
2023-10-11 10:40:44 +0000

    Update file

--- resources/views/_/app.blade.php
+++ resources/views/_/app.blade.php
@@ -7,12 +7,11 @@ data-assets-path="../assets/"
 data-template="vertical-menu-template-free" --}}
 
 <html lang="en"
-
-class="light-style layout-menu-fixed layout-compact"
-dir="ltr"
-data-theme="theme-default"
-data-assets-path="../assets/"
-data-template="vertical-menu-template-free"
+  class="light-style layout-menu-fixed layout-compact"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="../assets/"
+  data-template="vertical-menu-template-free"
 >
     <head>
         <meta charset="utf-8" />
